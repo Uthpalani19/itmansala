800274538a9cb95caa6e96b329835ecaffa5c1f2
Uthpalani19 <2020is022@stu.ucsc.cmb.ac.lk>
2022-11-15 19:06:49 +0530

    With nav and footer

---
+++ Interim/footer.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="https://kit.fontawesome.com/a87d6dd22b.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" type="text/css" href="css/global.css">
+</head>
+<body>
+<div class="footer-container" id="footer">
+            <div class="social">
+                <div class="left">
+                    <p>IT Mansala</p>
+                </div>
+                <div class="right">
+                    <i class="fa-brands fa-youtube"></i>
+                    <i class="fa-brands fa-linkedin"></i>
+                    <i class="fa-brands fa-square-twitter"></i>
+                    <i class="fa-brands fa-square-instagram"></i>
+                    <i class="fa-brands fa-facebook"></i>
+                </div>
+            </div>
+            <div class="privacy">
+                <div class="privacy-left">
+                    <p>Privacy Policy</p>
+                    <p>Terms & Conditions</p>
+                    <p>Cookies Policy</p>
+                    <p>Return Policy</p>
+                </div>
+                <div class="privacy-right">
+                    <p>Copyright <i class="fa-regular fa-copyright"></i> 2022, IT Mansala. All Rights Reserved.</p>
+                </div>
+
+            </div>
+        </div>
+
+
+</body>
+</html>
---
+++ Interim/navbar.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="https://kit.fontawesome.com/a87d6dd22b.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" type="text/css" href="css/global.css">
+</head>
+<body>
+
+    <div class="navbar" id="navId">
+        <div class="nav-left">
+            <img class="navbar-logo" src="images/icon.png">
+        </div>
+        <div class="nav-right">
+            <div class="nav-links">
+                <p>Dashboard</p>
+                <p>Payment</p>
+                <p>Student</p>
+                <p>Course</p>
+            </div>
+            <div class="nav-user-tray">
+                <p><i class="fa-regular fa-bell"></i></p>
+                <p><i class="fa-solid fa-circle-user"></i></p>
+                <p><?php echo $_SESSION['name']; ?></p>
+            </div>
+
+        </div>
+    </div>
+
+</body>
+</html>
