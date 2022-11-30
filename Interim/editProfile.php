b0b88044f2028bdf6d1cc49fd7fcb73f9cca83de
vinodh ramanayake <2020is080@stu.ucsc.cmb.ac.lk>
2022-12-01 00:05:31 +0530

    bageta type

--- Interim/editProfile.php
+++ Interim/editProfile.php
@@ -9,7 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/editProfile.css"></link>
-    <title>Edit Profile</title>
+    <title>Edit pro</title>
 </head>
 <body>
     <div class="container">
