180dba9d54ab6394e700fff2eca5cf3ad0094945
Uthpalani19 <2020is022@stu.ucsc.cmb.ac.lk>
2022-12-14 14:18:58 +0530

    logout added #1

--- Interim/viewDeletedQuestions.php
+++ Interim/viewDeletedQuestions.php
@@ -21,20 +21,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Delete Questions</title>
+    <title>Recover Questions</title>
 </head>
 
 <body>
     <?php
         require_once 'dbconnection.php';
 
-        $questionID = $_GET['deleteId'];
-        $sql = "UPDATE modelpaperquestion set status=0 WHERE questionId='$questionID'";
+        $questionID = $_GET['recoverId'];
+        $sql = "UPDATE modelpaperquestion set status=1 WHERE questionId='$questionID'";
         $result = mysqli_query($connection,$sql);
 
         if($result)
         {
-            echo "<script>window.location.href='viewAddedQuestions.php'</script>";        }
+            echo "<script>window.location.href='viewAddedQuestions.php'</script>";
+        }
         else
         {
             echo "Question Deletion Failed";
--- Interim/viewProfile.php
+++ Interim/viewProfile.php
@@ -1,3 +1,17 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['firstname']))
+    {
+        header('location:index.php');
+    }
+    if(isset($_GET['logout']))
+    {
+        session_destroy();
+        unset($_SESSION['firstname']);
+        header('location:index.php');
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
