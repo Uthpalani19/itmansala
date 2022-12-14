108da72341320c0ad9c61fd4e847ef4e981f5db2
Uthpalani19 <2020is022@stu.ucsc.cmb.ac.lk>
2022-12-15 02:39:22 +0530

    Answers - Check box added

--- Interim/editQuestions.php
+++ Interim/editQuestions.php
@@ -60,22 +60,22 @@
 
                         ?>
                                 <div class="question-box">
-                                    <textarea class="question-add" name="question" placeholder="Question" rows="4" cols="100" required><?php echo $question; ?></textarea>
-                                </div>
+                                    <textarea class="question-add" name="question" placeholder="Question" rows="4" cols="100" required><?php echo $question; ?></textarea>                                </div>
                                 <div>
                                     <textarea class="option" name="option1" placeholder="Option 1" rows="4" cols="60" required><?php echo $option1; ?></textarea>
+                                    <input type="radio" class="input-option" name="answer" value="option1" <?php echo ($answer==$option1)?'checked':'' ?>>
                                 </div>
                                 <div>
                                     <textarea class="option" name="option2" placeholder="Option 2" rows="4" cols="60" required><?php echo $option2; ?></textarea>
+                                    <input type="radio" class="input-option" name="answer" value="option2" <?php echo ($answer==$option2)?'checked':'' ?>>
                                 </div>
                                 <div>
                                     <textarea class="option" name="option3" placeholder="Option 3" rows="4" cols="60" required><?php echo $option3; ?></textarea>
+                                    <input type="radio" class="input-option" name="answer" value="option3" <?php echo ($answer==$option3)?'checked':'' ?>>
                                 </div>
                                 <div>
                                     <textarea class="option" name="option4" placeholder="Option 4" rows="4" cols="60" required><?php echo $option4; ?></textarea>
-                                </div>
-                                <div>
-                                    <textarea class="answer" name="answer" placeholder="Answer" rows="4" cols="60" required><?php echo $answer; ?></textarea>
+                                    <input type="radio" class="input-option" name="answer" value="option4" <?php echo ($answer==$option4)?'checked':'' ?>>
                                 </div>
 
                                 <!--div class="buttons"-->
@@ -91,15 +91,29 @@
                                         // Check if the options are not same
                                         if($_POST['option1'] != $_POST['option2'] && $_POST['option1'] != $_POST['option3'] && $_POST['option1'] != $_POST['option4'] && $_POST['option2'] != $_POST['option3'] && $_POST['option2'] != $_POST['option4'] && $_POST['option3'] != $_POST['option4'])
                                         {
-                                            // Check if the answer is one of the options
-                                            if($_POST['answer'] == $_POST['option1'] || $_POST['answer'] == $_POST['option2'] || $_POST['answer'] == $_POST['option3'] || $_POST['answer'] == $_POST['option4'])
-                                            {
                                                 $question = $_POST['question'];
                                                 $option1 = $_POST['option1'];
                                                 $option2 = $_POST['option2'];
                                                 $option3 = $_POST['option3'];
                                                 $option4 = $_POST['option4'];
                                                 $answer = $_POST['answer'];
+
+                                                if($answer == "option1")
+                                                {
+                                                    $answer = $option1;
+                                                }
+                                                else if($answer == "option2")
+                                                {
+                                                    $answer = $option2;
+                                                }
+                                                else if($answer == "option3")
+                                                {
+                                                    $answer = $option3;
+                                                }
+                                                else if($answer == "option4")
+                                                {
+                                                    $answer = $option4;
+                                                }
 
                                                 $sql = "UPDATE modelpaperquestion SET question = '$question', option1 = '$option1', option2 = '$option2', option3 = '$option3', option4 = '$option4', answer = '$answer' WHERE questionID = '$questionID'";
                                                 $result = mysqli_query($connection, $sql);
@@ -113,12 +127,6 @@
                                                     echo "<script>alert('Question Update Failed!')</script>";
                                                     echo "<script>window.location.href='viewAddedQuestions.php'</script>";
                                                 }
-                                            }
-                                            else
-                                            {
-                                                echo "<script>alert('Answer is not one of the options!')</script>";
-                                                echo "<script>window.location.href='viewAddedQuestions.php'</script>";
-                                            }
                                         }
                                         else
                                         {
