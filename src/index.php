931f3ed49dff48d227b2baf8a7d9ce28d0a95105
vinodh ramanayake <2020is080@stu.ucsc.cmb.ac.lk>
2023-03-05 22:51:49 +0530

    added comment #1

--- src/index.php
+++ src/index.php
@@ -104,6 +104,7 @@
     <?php include("views/landingPages/pi.php"); ?>
     </div>
 
+            <!-- this is jus a comment -->
     <div class="lpage3-course-des">
             <div class="course-des-heading">
                 <h3>01 තොරතුරු සහ සන්නිවේදන තාෂණය පිළිබඳ සංකල්ප</h3>
