1ab4058ffb819ebc0d42e162d56028e24cb03fbe
Uthpalani19 <2020is022@stu.ucsc.cmb.ac.lk>
2023-04-27 04:31:36 +0530

    Teacher table toggle buttn done

--- src/views/adminviews/viewTeachers.php
+++ src/views/adminviews/viewTeachers.php
@@ -9,24 +9,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Teacher Details </title>
     <script src="https://kit.fontawesome.com/a87d6dd22b.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="..\..\assets\css\viewTeachers.css">
 </head>
 
 <body>
     <br />
-    <!-- Search Teacher -->
-    <div class="container">
-        <div class="container-item">
-            <form class="searchBar" method="POST">
-            <p><input type="text" placeholder="Search Teachers"  class="search-bar" name="search">
-                <button type="submit"><i class="fa-solid fa-search"></i></button></p>
-            </form>
+    <!-- Top pane -->
+    <div class="teacher-options-container">
+        <!-- Add Teacher Button -->
+        <button class="addTeacher"><i class="fa fa-plus plus-icon" aria-hidden="true"></i><a href="addTeacher.php">Add new teacher</a> </button>
+
+        <!-- Search Teacher -->
+        <div class="container">
+            <div class="container-item">
+                <form class="searchBar" method="POST">
+                <p><input type="text" placeholder="Search Teachers"  class="search-bar" name="search">
+                    <button type="submit"><i class="fa-solid fa-search"></i></button></p>
+                </form>
+            </div>
         </div>
     </div>
 
     <!-- Table Content -->
     <div class="content">
-        <table>
+        <table class="tableStudent" id="student-details">
             <tr>
               <th>Teacher Image</th>
               <th>Name</th>
@@ -44,15 +51,24 @@
                 if(isset($_POST['search'])){
                     $search_text = $_POST['search'];
 
-                    $sql = "SELECT teacherImage,name,phoneNumber,email from teacher WHERE phoneNumber LIKE '%$search_text%' OR name LIKE '%$search_text%' OR email LIKE '%$search_text%'";
+                    $sql = "SELECT teacherImage,name,phoneNumber,email,status from teacher WHERE phoneNumber LIKE '%$search_text%' OR name LIKE '%$search_text%' OR email LIKE '%$search_text%'";
                 } else {
-                    $sql = "SELECT teacherImage,name,phoneNumber,email from teacher";
+                    $sql = "SELECT teacherImage,name,phoneNumber,email,status from teacher";
                 }
 
                 $result = mysqli_query($connection,$sql);
 
                 while($row = mysqli_fetch_assoc($result))
                 {
+                    if($row['status']== '1')
+                    {
+                        $checkStatus = "checked";
+                    }
+                    else
+                    {
+                        $checkStatus = "";
+                    }
+
                     echo '
                         <tr>
                             <td> <img class="teacherimg" src="'.$row['teacherImage'].'"</td>
@@ -61,7 +77,7 @@
                             <td>'.$row['email'].'</td>
                             <td>
                                 <label class="switch">
-                                <input type="checkbox" checked>
+                                <input type="checkbox" class="active-status" '.$checkStatus.' data-phone="'.$row['phoneNumber'].' ">
                                 <span class="slider round"></span>
                                 </label>
                             </td>
@@ -73,8 +89,26 @@
             ?>
         </table>
     </div>
-
-    <!-- Add Teacher Button -->
-    <button class="addTeacher"> <a href="addTeacher.php">Add new teacher</a> </button>
 </body>
+
+<!-- Ajax for active status -->
+<script type="text/javascript">
+    $(document).ready(function(){
+        $(".active-status").click(function(){
+            var input = $(this).val();
+            var isChecked = $(this).prop("checked"); // check if the checkbox is checked or not
+            var phoneNumber = $(this).data("phone"); 
+
+            $.ajax({
+                url: "../../config/adminconfig/teacherActiveStatus.php",
+                method: "POST",
+                data: {input:input, isChecked:isChecked, phoneNumber:phoneNumber},
+                success: function(){
+                    load("#student-details");
+                }
+            });
+        });
+    });
+</script>
+
 </html>
