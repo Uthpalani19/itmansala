9650ec48eda6b8cdd7b1da6486b0d3a054d2940b
sandaniii <amandipunchihewa@gmail.com>
2023-05-02 11:32:39 +0530

    admin bar chart

--- src/views/adminviews/viewStudents.php
+++ src/views/adminviews/viewStudents.php
@@ -1,4 +1,3 @@
-<!-- Navigation Bar -->
 <?php
     require '..\..\assets\includes\navbar-admin.php';
 ?>
@@ -8,79 +7,99 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Student Details </title>
+    <title> Teacher Details </title>
     <script src="https://kit.fontawesome.com/a87d6dd22b.js" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <link rel="stylesheet" href = "../../assets/css/viewStudents.css">
+    <link rel="stylesheet" href="..\..\assets\css\viewStudents.css">
 </head>
 
 <body>
-    <div class="box">
-        <br />
-        <!-- Search Student -->
-        <div class="container1">
-            <div class="container-item">
-                <form class="searchBar" method="POST">
-                    <p><input type="text" placeholder="Search Students" name="search" class="search-bar">
-                    <button type="submit"><i class="fa-solid fa-search"></i></button></p>
-                </form>
-            </div>
-        </div>
-
-        <!-- Table Content -->
-        <div class="content">
-            <table>
-                <tr>
-                <th>Name</th>
-                <th>Phone Number</th>
-                <!-- <th>Email</th> -->
-                <th>Active</th>
-                <!-- <th>Edit</th> -->
-                <th>Delete</th>
-                </tr>
-
-                <!-- PHP code -->
-                <?php
-                    require_once '../../config/dbconnection.php';
-
-                    if(isset($_POST['search'])) {
-                        $search_text = $_POST['search'];
-
-                        $sql = "SELECT phoneNumber, name FROM student WHERE name LIKE '%$search_text%' OR phoneNumber LIKE '%$search_text%'";
-                    }
-                    else {
-                        $sql = "SELECT phoneNumber, name, status FROM student";
-                    }
-
-                    $result = mysqli_query($connection, $sql);
-
-                    while($row = mysqli_fetch_assoc($result)) {
-                        echo '
-                            <tr>
-                                <td>'.$row['name'].'</td>
-                                <td>'.$row['phoneNumber'].'</td>
-                                <td>
-                                    <label class="switch">
-                                        <input type="checkbox" '.($row['status'] == 1 ? "checked" : "").' onchange="updateStatus(\''.$row['phoneNumber'].'\', this.checked)">
-                                        <span class="slider round"></span>
-                                    </label>
-                                </td>
-                                <td>
-                                    <form action="../../config/adminconfig/delete.php?deleteStuId='.$row['phoneNumber'].'" method="POST">
-                                        <button type="submit" class="delete-btn" onclick="return confirm(\'Are you sure you want to delete this student?\')">
-                                            <i class="fa-solid fa-trash" id="icons"></i>
-                                        </button>
-                                    </form>
-                                </td>
-                                
-                            </tr>
-                        ';
-                    }
-                ?>
-            </table>
+    <br />
+  
+    <!-- Search Student -->
+    <div class="container">
+        <div class="container-item">
+            <form class="searchBar" method="POST">
+            <p><input type="text" placeholder="Search Students"  class="search-bar" name="search">
+                <button type="submit"><i class="fa-solid fa-search"></i></button></p>
+            </form>
         </div>
     </div>
+   
+    <!-- Table Content -->
+    <div class="content">
+        <table class="tableStudent" id="student-details">
+            <tr>
+              <th>Name</th>
+              <th>Phone Number</th>
+              <th>Email</th>
+              <th>Active</th>
+              <th>Delete</th>
+            </tr>
 
-    <?php include('../../assets/includes/footer.php') ?>
+            <!-- PHP code -->
+            <?php
+                require_once '..\..\config\dbconnection.php';
+
+                if(isset($_POST['search'])){
+                    $search_text = $_POST['search'];
+
+                    $sql = "SELECT name,phoneNumber,email,status from student WHERE phoneNumber LIKE '%$search_text%' OR name LIKE '%$search_text%' OR email LIKE '%$search_text%'";
+                } else {
+                    $sql = "SELECT name,phoneNumber,email,status from student";
+                }
+
+                $result = mysqli_query($connection,$sql);
+
+                while($row = mysqli_fetch_assoc($result))
+                {
+                    if($row['status']== '1')
+                    {
+                        $checkStatus = "checked";
+                    }
+                    else
+                    {
+                        $checkStatus = "";
+                    }
+
+                    echo '
+                        <tr>
+                            <td>'.$row['name'].'</td>
+                            <td>'.$row['phoneNumber'].'</td>
+                            <td>'.$row['email'].'</td>
+                            <td>
+                                <label class="switch">
+                                <input type="checkbox" class="active-status" '.$checkStatus.' data-phone="'.$row['phoneNumber'].' ">
+                                <span class="slider round"></span>
+                                </label>
+                            </td>
+                            <td><a href="../../config/adminconfig/delete.php?deleteStuId='.$row['phoneNumber'].'"><i class="fa-solid fa-trash" id="icons"></i></a></td>
+                        </tr>
+                    ';
+                }
+            ?>
+        </table>
+    </div>
 </body>
+
+<!-- Ajax for active status -->
+<script type="text/javascript">
+    $(document).ready(function(){
+        $(".active-status").click(function(){
+            var input = $(this).val();
+            var isChecked = $(this).prop("checked"); // check if the checkbox is checked or not
+            var phoneNumber = $(this).data("phone"); 
+
+            $.ajax({
+                url: "../../config/adminconfig/studentActiveStatus.php",
+                method: "POST",
+                data: {input:input, isChecked:isChecked, phoneNumber:phoneNumber},
+                success: function(){
+                    load("#student-details");
+                }
+            });
+        });
+    });
+</script>
+
 </html>
