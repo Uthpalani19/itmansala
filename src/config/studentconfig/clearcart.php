2b14ffeee05ca4b69878d776e20c7593a2cedadc
Uthpalani19 <2020is022@stu.ucsc.cmb.ac.lk>
2023-05-11 11:52:32 +0530

    Admin dashboard done

--- src/config/studentconfig/clearcart.php
+++ src/config/studentconfig/clearcart.php
@@ -5,8 +5,8 @@ $PhoneNumber = $_SESSION["studentphone"];
 $time = time();
 foreach($_SESSION['cart'] as $cid){
     $element = $cid;
-    $query = "INSERT INTO student_course (phoneNumber, courseId, lastaccesstime, status) 
-    VALUES('$PhoneNumber', '$element', '$time', '0')";
+    $query = "INSERT INTO student_course (phoneNumber, courseId, status) 
+    VALUES('$PhoneNumber', '$element', '1')";
     mysqli_query($connection, $query);
 }
 unset($_SESSION['cart']);
